49bb03b1c79a56c128c8ffa763e5ad8cdbe406a9
Alex Dombrovski <dombrovskia@upmc.edu>
2022-04-28 16:37:21 -0400

    Added screen.lmer for lme4

--- screen.lmerTest.R
+++ screen.lmerTest.R
@@ -5,3 +5,11 @@ screen.lmerTest <- function (mod,p=NULL) {
   names(dd)[3] <- 'VIF'
   dd$`Pr(>|t|)` <- as.numeric(dd$`Pr(>|t|)`)
   print(dd[dd$`Pr(>|t|)`<p,c(1,3)], digits = 3)}
+
+screen.lmer <- function (mod,stat=NULL) {
+  if (is.null(stat)) {stat <- 3}
+  c1 <- as.data.frame(coef(summary(mod))[,3])
+  dd <- cbind(c1[2:nrow(c1),],as.data.frame(vif.lme(mod)))
+  names(dd) <- c('t stat', 'VIF')
+  dd$tstat <- as.numeric(dd$`t stat`)
+  print(dd %>% filter(abs(tstat) > stat) %>% select(1:2), digits = 3)}
